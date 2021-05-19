8472c0185a2070dccaed546df3c4c572131610b5
ghkddyto <dyto153@naver.com>
2021-05-20 02:00:26 +0900

    프로그래머스 레벨1 행렬덧셈
    https://programmers.co.kr/learn/courses/30/lessons/12950

---
+++ src/level1_add_matrix.java
@@ -0,0 +1,20 @@
+//2개의 행렬 arr1과 arr2를 입력받아,
+//행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+
+import java.util.Arrays;
+public class level1_add_matrix {
+    public static void main(String[] args) {
+        int[][] arr1 = {{1,2},{2,3}};
+        int[][] arr2 = {{3,4},{5,6}};
+
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
